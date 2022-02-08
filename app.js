448c8c77f930bf26c7f6f372f230438964d660de
xelos97 <rakshit876@icloud.com>
2022-02-08 08:43:20 +0530

    update 1/8 JS

--- app.js
+++ app.js
@@ -6,15 +6,24 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandText = document.querySelector('[previous-operand]')
 const currentOperandText = document.querySelector('[current-operand]')
 
+
+const calculator = new Calculator(previousOperandText, currentOperandText)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click')
+})
+
 class Calculator {
     constructor(previousOperandText, currentOperandText) {
         this.previousOperandText = previousOperandText
         this.currentOperandText = currentOperandText
+        this.clear ()
     }
 
     clear() {
         this.currentOperand = ''
-        this.
+        this.previousOperand = ''
+        this.operation = undefined
 
     }
 
